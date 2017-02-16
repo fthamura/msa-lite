356bb4408ec68216f86efe776ed831669bbd4d09
fthamura <frans@meruvian.org>
2017-02-16 21:53:38 +0700

    add springbootapp

--- msa-sboot/src/main/java/org/meruvian/workshop/Application.java
+++ msa-sboot/src/main/java/org/meruvian/workshop/Application.java
@@ -2,25 +2,32 @@ package org.meruvian.workshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.bind.RelaxedPropertyResolver;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
+import org.meruvian.workshop.jaxrs.Main;
 
 /**
- * @author Dave Syer
+ * @author Meruvian
  */
 @SpringBootApplication
 @RestController
-public class Application {
+public class Application  extends SpringBootServletInitializer  implements WebApplicationInitializer {
 
 	@RequestMapping("/")
 	public String home() {
 		return "Hello Docker World";
 	}
 
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(Application.class);
+	}
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+	public static void main(String[] args) throws Exception {
+	        SpringApplication.run(Application.class, args);
 	}
 
 }
